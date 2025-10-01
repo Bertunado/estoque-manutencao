132997e5d57e4b659f9fb94881ad0484814864c5
Bernardo <bernardoviana2005@gmail.com>
2025-10-01 11:15:25 -0300

    Adiciona layout responsivo e corrige base.html

--- app_estoque/static/js/main.js
+++ app_estoque/static/js/main.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    const menuBtn = document.getElementById('menu-btn');
+    const sidebar = document.getElementById('sidebar');
+
+    if (menuBtn && sidebar) {
+        menuBtn.addEventListener('click', () => {
+            sidebar.classList.toggle('-translate-x-full');
+        });
+    }
+    
+
     // Função auxiliar para pegar o CSRF token, necessária em vários blocos
     function getCookie(name) {
         let cookieValue = null;
--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = 'django-insecure-qiaw$l%0dqe9kfp!jddzhhp2(-zxdhv)^!2fifn)ca$c)#mm_r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
